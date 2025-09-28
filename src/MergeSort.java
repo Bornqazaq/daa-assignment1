f7ca3e495fe709a4bd90abcb7328d3de7bd5481a
Bornqazaq <242606@astanait.edu.kz>
2025-09-28 19:29:03 +0500

    feat(mergesort): added cutoff with insertion sort for arrays <= 10

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -1,7 +1,12 @@
 public class MergeSort {
 
+    private static final int CUTOFF = 10;
+
     public static void mergeSort(int[] arr, int left, int right) {
-        if (left >= right) return;
+        if (right - left + 1 <= CUTOFF) {
+            insertionSort(arr, left, right);
+            return;
+        }
 
         int mid = left + (right - left) / 2;
 
@@ -42,6 +47,18 @@ public class MergeSort {
 
         for (int num : arr) {
             System.out.print(num + " ");
+        }
+    }
+
+    private static void insertionSort(int[] arr, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= left && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
         }
     }
 }
